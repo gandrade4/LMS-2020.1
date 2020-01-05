0f50f5657542dc31bae96355130cbfe3488833ff
Gabriel <gandrade4@alu.ufc.br>
2020-01-05 00:13:21 -0300

    Alinhamento dos menus e css das setas e rodinhas

--- PraticandoLMS/js/main.js
+++ PraticandoLMS/js/main.js
@@ -1,1 +1,9 @@
-$('.main-slider').slick();
+$('.main-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
