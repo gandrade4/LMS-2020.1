0539fb4953c818da2ccefd36e48e025b41acf3fd
Gabriel <gandrade4@alu.ufc.br>
2020-10-18 11:39:24 -0300

    login pronto

--- Atividade 03/js/main.js
+++ Atividade 03/js/main.js
@@ -1,10 +1,20 @@
+var token = "";
+
 //aparecer botão de comprar com o clique
 function configurarEventos(){
+    var form = document.getElementById("form-login");
+    var sair = document.getElementById("botao-sair");
     var product = document.getElementsByClassName("product");
         for(var i=0; i<product.length; i++){
             product[i].addEventListener("click", handleClickCard);
-
+            
         }
+        sair.addEventListener("click", logout);
+        form.addEventListener("submit", function(event){
+            event.preventDefault();
+            login(this);
+           
+        });
 }
 
 function handleClickCard(){
@@ -21,51 +31,81 @@ function hideCard(){
     document.getElementsByClassName("product").style.visibility = "hidden";
 }
 
+//ocultar botão log out e sigh in
+function hide(){
+    var entrar = document.getElementById("botao-entrar");
+    var sair = document.getElementById("botao-sair");
+    if(token == ""){
+        console.log(token);
+        entrar.style.display = "block";
+        sair.style.display = "none";
+    }else{
+        entrar.style.display = "none";
+        sair.style.display = "block";
+    }   
+}
+function logout(){
+    token = "";
+    hide();
+    alert("Você saiu com sucesso!");
+}
 
-function request(metodo, url, cFunction, status, dados){
+
+//login
+
+function requisicaoAPI(url, metodo, funcao, dados, status){
     if(status == undefined){
         status = 200;
     }
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if(xhttp.readyState == 4 && xhttp.status == status){
-            cFunction(this);
+          funcao(this);
         }
     };
     xhttp.open(metodo, url, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json');
     if(dados == undefined){
         xhttp.send();
     }else{
         xhttp.send(JSON.stringify(dados));
+    } 
+}
+
+function login(form){
+    if(verificarLogin(form)){
+        var dados = {
+            email: form["email"].value,
+            password: form["password"].value
+        };
+        form.reset();
+        requisicaoAPI("https://reqres.in/api/login", "POST", responseLogin, dados);
     }
-    
 }
 
-function getloginInfo(){
-    var url = "https://reqres.in/api/users";
-    request("GET", url, loginInfo);
-}
+function verificarLogin(form){
+    var email = form["email"].value;
+    var password = form["password"].value;
+  
+    if(email.length == 0){
+        alert("E-mail incorreto. Tente novamente!");
+        return false;
+    }
+    if(password.length == 0){
+        alert("Senha incorreta. Tente novamente!");
+        return false;
+    }
+    return true;
+  }
 
-function loginInfo(xhttp){
+  function responseLogin(xhttp){
     var response = JSON.parse(xhttp.responseText);
-
-    document.getElementById("qtd-usuarios").innerHTML = response.total;
-
-    document.getElementById("qtd-paginas").innerHTML = response.total_pages;
-
-}
-
-function carregarTabela(elemento){
-    var pagina = elemento.getAttribute("data-pagina");
-
-    var url = "https://reqres.in/api/users?page=" + pagina;
-    request("GET", url, responseCarregarTabela);
-}
-
-function criarElementoTexto(tag, texto){
-    var elemento = document.createElement(tag);
-    var noTexto = document.createTextNode(texto);
-    elemento.appendChild(noTexto);
-
-    return elemento;
+    if(response.token == ""){
+        alert(response.error);
+    }else{
+        token = response.token;
+        console.log(token);
+        alert("Você entrou com sucesso!");
+    }
+    hide();
 }
